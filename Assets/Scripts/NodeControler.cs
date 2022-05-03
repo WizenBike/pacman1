7555b3ee9d1e43bd3a1fce60b65f86b59214705f
AndoMiz <95219405+AndoMiz@users.noreply.github.com>
2022-05-03 10:46:32 +0200

    Upravy duchov

--- Assets/Scripts/NodeControler.cs
+++ Assets/Scripts/NodeControler.cs
@@ -16,10 +16,14 @@ public class NodeControler : MonoBehaviour
     public GameObject nodeDown;
     public GameObject nodeRight;
     public GameObject nodeLeft;
+    public bool startNode = false;
 
     // Start is called before the first frame update
     void Start()
     {
+       if (startNode == false)
+       {
+
         print("aede");
         RaycastHit2D[] hitDown;
         hitDown = Physics2D.RaycastAll(transform.position, -Vector2.up);
@@ -65,13 +69,66 @@ public class NodeControler : MonoBehaviour
             }
         }
 
+        }
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (startNode == true)
+        {
+            print("aede");
+            RaycastHit2D[] hitDown;
+            hitDown = Physics2D.RaycastAll(transform.position, -Vector2.up);
+            for (int i = 0; i < hitDown.Length; i++)
+            {
+                if (1 >= Mathf.Abs(hitDown[i].point.y - transform.position.y) && hitDown[i].collider.gameObject.CompareTag("Gulicka"))
+                {
+                    canMoveDown = true;
+                    nodeDown = hitDown[i].collider.gameObject;
+                    hitDown[i].collider.gameObject.GetComponent<NodeControler>().nodeUp = gameObject;
+                }
+            }
+
+            RaycastHit2D[] hitUp;
+            hitUp = Physics2D.RaycastAll(transform.position, Vector2.up);
+            for (int i = 0; i < hitUp.Length; i++)
+            {
+                if (1 >= Mathf.Abs(hitUp[i].point.y - transform.position.y) && hitUp[i].collider.gameObject.CompareTag("Gulicka"))
+                {
+                    canMoveUp = true;
+                    nodeUp = hitUp[i].collider.gameObject;
+                    hitUp[i].collider.gameObject.GetComponent<NodeControler>().nodeDown = gameObject;
+                }
+            }
+
+            RaycastHit2D[] hitLeft;
+            hitLeft = Physics2D.RaycastAll(transform.position, -Vector2.right);
+            for (int i = 0; i < hitLeft.Length; i++)
+            {
+                if (1 >= Mathf.Abs(hitLeft[i].point.x - transform.position.x) && hitLeft[i].collider.gameObject.CompareTag("Gulicka"))
+                {
+                    canMoveLeft = true;
+                    nodeLeft = hitLeft[i].collider.gameObject;
+                    hitLeft[i].collider.gameObject.GetComponent<NodeControler>().nodeRight = gameObject;
+                }
+            }
+
+            RaycastHit2D[] hitRight;
+            hitRight = Physics2D.RaycastAll(transform.position, Vector2.right);
+            for (int i = 0; i < hitRight.Length; i++)
+            {
+                if (1 >= Mathf.Abs(hitRight[i].point.x - transform.position.x) && hitRight[i].collider.gameObject.CompareTag("Gulicka"))
+                {
+                    canMoveRight = true;
+                    nodeRight = hitRight[i].collider.gameObject;
+                    hitRight[i].collider.gameObject.GetComponent<NodeControler>().nodeLeft = gameObject;
+                }
+            }
+            startNode = false;
+        }  
     }
 
     public GameObject getNodeFromDirection(string direction) 
